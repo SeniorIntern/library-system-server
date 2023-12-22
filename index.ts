db62e20b9a1338d4fe7fb502383fa79feec986aa
SeniorIntern <tnikhil.official@gmail.com>
2023-12-22 22:57:56 +0545

    Desing book modal

--- index.ts
+++ index.ts
@@ -1,6 +1,27 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded())
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    requried: true
+  },
+  image_url: {
+    type: String,
+    required: true
+  }
+})
+
+const Book = mongoose.model('book', bookSchema)
 
 const port = 3000
 app.listen(port, async () => {
