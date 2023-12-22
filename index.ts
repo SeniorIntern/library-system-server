d8112beb741cc8906d8563cb97f94646576572cd
SeniorIntern <tnikhil.official@gmail.com>
2023-12-22 21:44:05 +0545

    Initial commit

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log('Hello, TypeScript!');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('hey');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
