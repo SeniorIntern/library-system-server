102856532ca418ac80312bbb77d40b31784b52a5
SeniorIntern <tnikhil.official@gmail.com>
2023-12-23 21:35:29 +0545

    Organize project structure

--- index.ts
+++ index.ts
@@ -1,27 +1,21 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import languages from './routes/languages'
+import authors from './routes/authors'
+import categories from './routes/categories'
+import books from './routes/books'
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded())
+app.use('/api/languages', languages)
+app.use('/api/authors', authors)
+app.use('/api/categories', categories)
+app.use('/api/books', books)
 
-const bookSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    requried: true
-  },
-  image_url: {
-    type: String,
-    required: true
-  }
-})
-
-const Book = mongoose.model('book', bookSchema)
+mongoose.connect('mongodb://127.0.0.1:27017/library')
+  .then(() => console.log('Connected!'));
 
 const port = 3000
 app.listen(port, async () => {
