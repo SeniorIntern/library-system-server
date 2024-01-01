4f426bbf3f205228e7848980450bdba3759c57a6
SeniorIntern <tnikhil.official@gmail.com>
2024-01-01 17:36:07 +0545

    Add route handler for POST book

--- routes/rentals.ts
+++ routes/rentals.ts
@@ -1,18 +1,13 @@
 import express from "express";
 import Rental from "../models/rental";
-import { Book } from '../models/book'
+import Book from "../models/book";
 const router = express.Router();
-
-router.get("/", async (req, res) => {
-  const rentals = await Rental.find().sort("-dateOut");
-  res.send(rentals);
-});
 
 router.post("/", async (req, res) => {
   const book = await Book.findById(req.body.bookId);
   if (!book) return res.status(400).send('Book not found!')
 
-  const { name, address, email, dailyRentalRate } = req.body
+  const { name, address, email } = req.body
   const rental = new Rental({
     customer: {
       name: name,
@@ -22,21 +17,10 @@ router.post("/", async (req, res) => {
     book: {
       _id: book._id,
       title: book.title,
-      dailyRentalRate: parseInt(dailyRentalRate)
     }
   });
 
   await rental.save()
-
-  res.send(rental);
-});
-
-router.get("/:id", async (req, res) => {
-  const rental = await Rental.findById(req.params.id);
-
-  if (!rental)
-    return res.status(404).send("The rental with the given ID was not found.");
-
   res.send(rental);
 });
 
