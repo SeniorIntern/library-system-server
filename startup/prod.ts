545bc550cd43ffe1672baa04715a736475e370f9
SeniorIntern <tnikhil.official@gmail.com>
2024-01-11 20:02:29 +0545

    Set up for production

---
+++ startup/prod.ts
@@ -0,0 +1,8 @@
+import { Application } from 'express'
+import helmet from 'helmet'
+import compression from 'compression'
+
+export default function(app: Application) {
+  app.use(helmet())
+  app.use(compression());
+};
